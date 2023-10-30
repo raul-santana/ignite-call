f8e841ceb7b853fbdcdc4f004e520a9fba35be3d
Raul Santana <ti.raul77@gmail.com>
2023-10-30 17:03:55 -0300

    style: add Roboto font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Heading } from "@ignite-ui/react";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Heading>Hello World</Heading>
     </div>
   )
 }
