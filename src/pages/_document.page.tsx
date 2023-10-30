0ad91021434b921249ec7a49b86692e556b4cdc6
Raul Santana <ti.raul77@gmail.com>
2023-10-30 19:00:13 -0300

    style: run lint

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -6,11 +6,21 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
 
-        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }}/>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
       </Head>
       <body>
         <Main />
