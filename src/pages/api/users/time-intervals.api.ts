3708dc4eb01a76f18e8df68aed924a0efdfecf90
Raul Santana <ti.raul77@gmail.com>
2023-11-08 23:42:52 -0300

    feat: persisting intervals in user_time_intervals table

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,6 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from 'next-auth'
 import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
+
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
 
 export default async function handle(
   req: NextApiRequest,
@@ -16,5 +28,26 @@ export default async function handle(
     buildNextAuthOptions(req, res),
   )
 
-  return res.json({ session })
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const { intervals } = timeIntervalsBodySchema.parse(req.body) // parse retornará erro caso a tipagem não esteja correta, caso quisessemo tratar esse erro aqui, através de algum if, poderíamos utilizar o safeparse
+
+  // await prisma.userTimeInterval.createMany() // o createMany não funciona no sqlLite, ele não aceita inserções multiplas, porém se estivessemos em outro bd poderíamos utilizar ele.
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user?.id,
+        },
+      })
+    }),
+  )
+
+  return res.status(201).end()
 }
