61217338b7f38cdf7a91d780b272b649691f5f1a
Raul Santana <ti.raul77@gmail.com>
2023-11-08 23:13:41 -0300

    feat: get session data and ensure user is authenticated on submitting form on TimeIntervalsComponent

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -22,6 +22,7 @@ import {
   IntervalItem,
 } from './styles'
 import { convertTimeStringToMinutes } from '@/utils/convert-time-string-to-minutes'
+import { api } from '@/lib/axios'
 
 const timeIntervalsFormSchema = z.object({
   intervals: z
@@ -96,7 +97,11 @@ export default function TimeIntervals() {
   const intervals = watch('intervals')
 
   async function handleSetTimeIntervals(data: TimeIntervalsFormOutPut) {
-    console.log(data)
+    const { intervals } = data
+
+    await api.post('/users/time-intervals', {
+      intervals,
+    })
   }
 
   return (
