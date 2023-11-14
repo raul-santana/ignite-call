6272e688a5e6d21ad307f18862bdb3b7f7dd5503
Raul Santana <ti.raul77@gmail.com>
2023-11-13 23:23:00 -0300

    feat: add navegation in month/year in header on Calendar component

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -8,22 +8,43 @@ import {
   CalendarHeader,
   CalendarTitle,
 } from './styles'
+import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1)
+  })
+
+  function handlePreviousMonth() {
+    const previousMonthDate = currentDate.subtract(1, 'month')
+
+    setCurrentDate(previousMonthDate)
+  }
+
+  function handleNextMonth() {
+    const previousMonthDate = currentDate.add(1, 'month')
+
+    setCurrentDate(previousMonthDate)
+  }
+
   const shortWeekDays = getWeekDays({ short: true })
+
+  const currentMonth = currentDate.format('MMMM')
+  const currentYear = currentDate.format('YYYY')
 
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Dezembro <span>2022</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
 
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="Previous Month">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Next Month">
             <CaretRight />
           </button>
         </CalendarActions>
